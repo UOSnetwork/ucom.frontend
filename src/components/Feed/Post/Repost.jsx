6bb8e09b511ab73931e3ee6063a8a486cd3105c6
Pavel Ivanov <sudokey@gmail.com>
2019-05-30 12:32:25 +0300

    Fixed reposts, fix #304

--- src/components/Feed/Post/Repost.jsx
+++ src/components/Feed/Post/Repost.jsx
@@ -65,7 +65,7 @@ const Repost = ({
           />
         ) : (
           <PostFeedContent
-            post={post}
+            post={post.post}
             postId={post.post.id}
             userId={post.post.user.id}
           />
