e2b400258ec160894072f1b982c9339e03f06e40
Pavel Ivanov <sudokey@gmail.com>
2019-07-12 16:35:59 +0300

    Remove old code

--- src/components/PostMedia/PostsGroupMain.jsx
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import PostsGroup from './PostsGroup';
-
-export default connect(
-  state => ({
-    posts: state.mainPostGroup.posts,
-  }),
-  null,
-)(PostsGroup);
